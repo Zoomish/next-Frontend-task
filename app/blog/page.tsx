0f647928adbd3809ed7f3212bcdabde41ae7fd63
Zoom <egor147536987@gmail.com>
2024-01-23 21:33:03 +0300

    Fix fetching posts API and handle errors. (generated by blackbox.ai )

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,18 +5,21 @@ import { useEffect, useState } from "react";
 import LoadingPosts from "./loading";
 
 async function getData(num: number) {
-  if (num > 2||num<1) {
-    num = 1
+  try {
+    const response = await fetch(`https://taxivoshod.ru/testapi/?w=list&page=${num}`, {
+      next: {
+        revalidate: 60,
+      },
+    });
+
+    if (!response.ok) throw new Error("Unable to fetch posts!");
+
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    getData(num=1)
   }
-  const response = await fetch(`https://taxivoshod.ru/testapi/?w=list&page=${num}`, {
-    next: {
-      revalidate: 60,
-    },
-  });
 
-  if (!response.ok) throw new Error("Unable to fetch posts!");
-
-  return response.json();
 }
 
 export const metadata: Metadata = {
@@ -34,29 +37,32 @@ export default async function Blog() {
       setPosts(data.items)
     };
     getPosts()
-
   }, [num])
-  
+
 
   return (
-    <>
-      <h1>Blog page</h1>
-      <div>
-        {posts.length != 0
-          ? <div>
-              <ul>
-                {posts.map((post: any) => (
-                  <li key={post.id}>
-                    <Link href={`/blog/${post.id}`}>{post.name}</Link>
-                  </li>
-                ))}
-              </ul>
-              <button onClick={() => setNum(num - 1)}>Previous</button>
-              <button onClick={() => setNum(num + 1)}>Next</button>
-            </div>
-          : LoadingPosts()
-        }
-      </div>
-    </>
+    <div>
+      {posts.length != 0
+        ? <div>
+          <h1>Blog page</h1>
+          <ul>
+            {posts.map((post: any) => (
+              <li key={post.id}>
+                <Link href={`/blog/${post.id}`}>{post.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <button onClick={() => {
+            setNum(num - 1)
+            setPosts([])
+          }}>Previous</button>
+          <button onClick={() => {
+            setNum(num + 1)
+            setPosts([])
+          }}>Next</button>
+        </div>
+        : LoadingPosts()
+      }
+    </div>
   );
 }
