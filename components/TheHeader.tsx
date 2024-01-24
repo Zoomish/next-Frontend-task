5e4d605feff62233e073b15c80b9987cf79ed322
Zoom <egor147536987@gmail.com>
2024-01-24 19:43:23 +0300

    Reorganize the file structure and code for the blog page. (generated by blackbox.ai )

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 const TheHeader = () => {
   return (
     <header>
-      <Link href="/">Home</Link>
-      <Link href="/blog">Blog</Link>
+      <Link href="/">Items</Link>
     </header>
   );
 };
